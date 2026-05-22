6596552a2134b5658b76448e24a2425bb2147aa8
rifat-sultana <rifatsultana88.web@gmail.com>
2026-05-22 21:38:11 +0600

    added middleware to deal with token encoding and decoding task

---
+++ middleware/authenticate.js
@@ -0,0 +1,19 @@
+const { verifyToken } = require("../auth/jwt");
+
+function authenticate(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Invalid or expired token" });
+  }
+}
+
+module.exports = { authenticate };
