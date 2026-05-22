69af12f6b7dd14543903877ba0fc2966965bda6c
rifat-sultana <rifatsultana88.web@gmail.com>
2026-05-22 21:36:44 +0600

    Refactor authentication setup to use async imports and enhance configuration options

--- auth/auth.js
+++ auth/auth.js
@@ -1,30 +1,33 @@
-// auth/auth.js
-const { betterAuth } = require("better-auth");
-const { mongodbAdapter } = require("better-auth/adapters/mongodb");
 const dotenv = require("dotenv");
-
 const { db } = require("../config/db")
 
 dotenv.config();
 
-const auth = betterAuth({
-  database: mongodbAdapter(db),
-  secret: process.env.BETTER_AUTH_SECRET,
-  emailAndPassword: {
-    enabled: true,
-  },
+async function createAuth() {
+  const { betterAuth } = await import("better-auth");
+  const { mongodbAdapter } = await import("better-auth/adapters/mongodb");
 
-  session: {
-    expiresIn: 60 * 60 * 24 * 7,
-    updateAge: 60 * 60 * 24,
-  },
+  return betterAuth({
+    database: mongodbAdapter(db),
+    secret: process.env.BETTER_AUTH_SECRET,
+    trustedOrigins: ["http://localhost:3000"],
+    emailAndPassword: {
+      enabled: true,
+    },
 
-  // google: {
-  //   prompt: "select_account",
-  //   clientId: process.env.GOOGLE_CLIENT_ID,
-  //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  //   redirectURI: "http://localhost:3001/api/auth/callback/google",
-  // },
-});
+    session: {
+      expiresIn: 60 * 60 * 24 * 7,
+      updateAge: 60 * 60 * 24,
+    },
 
-module.exports = { auth };
+    socialProviders: {
+      google: {
+        prompt: "select_account",
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      },
+    },
+  });
+}
+
+module.exports = { createAuth };
